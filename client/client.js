47b006e51a48129c59cf65d696fccba3ac7777c6
Sanni Michael Tomiwa <sannimichaelse@gmail.com>
2020-05-24 04:50:08 +0100

    initial commit to node-grpc

---
+++ client/client.js
@@ -0,0 +1,20 @@
+const grpc = require("grpc");
+const protoLoader = require("@grpc/proto-loader");
+
+const PROTO_PATH = "./customers.proto";
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    arrays: true,
+});
+
+const CustomerService = grpc.loadPackageDefinition(packageDefinition)
+    .CustomerService;
+const client = new CustomerService(
+    "localhost:30043",
+    grpc.credentials.createInsecure()
+);
+
+module.exports = client;
